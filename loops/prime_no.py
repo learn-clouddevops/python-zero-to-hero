01b73bbb71f5f026055a20910161bd24c365d27e
Payal Gupta <clouddevopswidpayal@gmail.com>
2026-04-24 09:10:34 +0530

    Create prime_no.py

---
+++ loops/prime_no.py
@@ -0,0 +1,10 @@
+#prime nos.
+
+for num in range (1,20):
+    if num > 1:
+        for i in range (2,num):
+            if (num%i == 0):
+                break
+        else:
+            print(f"{num} is a prime no.")
+            
