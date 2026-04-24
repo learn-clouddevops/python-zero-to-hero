9a5ba4d417ca99a19cde5710d4dd180709ee1ca0
Payal Gupta <clouddevopswidpayal@gmail.com>
2026-04-24 10:34:06 +0530

    Create grades_list.py

---
+++ loops/grades_list.py
@@ -0,0 +1,11 @@
+grades = [10,40,50,60]
+
+
+grades.append(95)
+
+average_grade = sum(grades) / len(grades)
+print(f"average grade is: {average_grade:.3f}")
+
+higest_grade = print(max(grades))
+    
+lowest_grade = min(grades)
