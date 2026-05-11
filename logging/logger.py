049ef7be72fafaa39df3a44a6ccc19d92c44d9f2
Payal Gupta <clouddevopswidpayal@gmail.com>
2026-05-11 11:10:45 +0530

    Create logger.py

---
+++ logging/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# #LOG MESSAGES
+
+# logging.debug("This is a debug message")
+# logging.info("This is an info message")
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+
+
